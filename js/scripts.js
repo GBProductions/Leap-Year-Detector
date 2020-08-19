611207c1ed462e88222cf078c23e963e3e23534f
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-19 16:30:26 -0700

    Remove lines of JS. Fix HTMl (spans).
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-    $("form#form").submit(function(event){
+    $("form#form").submit(function(event) {
       event.preventDefault();
       const userYear = parseInt($("input#leapYear").val());
 
-
-  function isLeapYear(userYear) {
     if ((userYear % 4 === 0) && (userYear % 100 !== 0) || (userYear % 400 === 0)){
     $(".year1").show();
   } else {
     $(".notLeapYear").show();
   }
-
+  });
+});
