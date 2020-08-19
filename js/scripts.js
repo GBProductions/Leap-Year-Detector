df3a1697dc04d7b027ea06f0a2db55fc9abb2b26
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-19 15:43:02 -0700

    Add JS if/else for .year1 and .notLepYear. Add CSS for button.
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 $(document).ready(function(event){
-  $("form#form").submit(function(event){
-    event.preventDefault();
-    
-    const userYear = parseInt("input#leapYear").val();
-  
-  $("").text(userYear);   
+    $("form#form").submit(function(event){
+      event.preventDefault();
+      
+      const userYear = parseInt($("input#leapYear").val());
+
+  function isLeapYear(userYear) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+
+    $(".year1").show();
+      return true;
+  } else {
+    $(".notLeapYear").show();
+    return false;
+  }
+  };
+  console.log(userYear);
+  $(".year1").text("is a leap year!");
+  $(".notLeapYear").text("is not a leap year.");
+
   });
-
-
-
-
-
-// Business Logic --------
-function isLeapYear(userYear) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
-    return true;
-} else {
-  return false;
-};
-
 });
-
