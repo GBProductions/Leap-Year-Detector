0b1182dd46ef19c872ec982dfa3c80ca5353a12d
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-19 16:19:26 -0700

    Delete extra code in JS script.
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,13 @@
-$(document).ready(function(event){
+$(document).ready(function(){
     $("form#form").submit(function(event){
       event.preventDefault();
-      
       const userYear = parseInt($("input#leapYear").val());
-    });
-  });
+
 
   function isLeapYear(userYear) {
     if ((userYear % 4 === 0) && (userYear % 100 !== 0) || (userYear % 400 === 0)){
     $(".year1").show();
   } else {
     $(".notLeapYear").show();
-    return false;
   }
-}
+
