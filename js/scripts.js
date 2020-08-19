22d112cc355c2189fa644e0296f5c2e3859f960f
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-19 15:07:32 -0700

    add more JS, build perameters.
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function(event){
   $("form#form").submit(function(event){
     event.preventDefault();
-
+    
+    const userYear = parseInt("input#leapYear").val();
+  
+  $("").text(userYear);   
   });
+
+
+
+
+
+// Business Logic --------
+function isLeapYear(userYear) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+    return true;
+} else {
+  return false;
+};
+
 });
+
