1192377bdaa7c8203ddb0e8b69f28a7602ffc7f7
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-19 15:52:37 -0700

    Adjust punctuation for JS.
    Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,18 @@ $(document).ready(function(event){
       event.preventDefault();
       
       const userYear = parseInt($("input#leapYear").val());
+    });
+  });
 
   function isLeapYear(userYear) {
-    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+    if ((userYear % 4 === 0) && (userYear % 100 !== 0) || (userYear % 400 === 0)){
 
     $(".year1").show();
+    $(".year1").text("is a leap year!");
       return true;
   } else {
     $(".notLeapYear").show();
+    $(".notLeapYear").text("is not a leap year.");
     return false;
   }
-  };
-  console.log(userYear);
-  $(".year1").text("is a leap year!");
-  $(".notLeapYear").text("is not a leap year.");
-
-  });
 });
